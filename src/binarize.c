8a03f998ebde213f1a8be6d6fd170aea6991d231
NNigmat <nnigmat@gmail.com>
2018-05-26 13:48:41 +0300

    + binarize

--- src/binarize.c
+++ src/binarize.c
@@ -71,16 +71,84 @@ void one_complement(char *b)
 
 void two_complement(char *b)
 {
-    /** YOUR CODE HERE */
+    int i = 0;
+    while (b[i] != '\0'){
+        if(b[i] == '1'){
+            b[i] = '0';
+        } else {
+            b[i] = '1';
+        }
+        ++i;
+    }
+
+    --i;
+
+    // Add 1
+    int carryIn = 0;
+    for (; i >= 0; --i) {
+        if (b[i] != '1') {
+            break;
+        } else {
+            if (carryIn == 0) {
+                carryIn = 1;
+                b[i] = '0';
+            } else {
+                b[i] = '1';
+            }
+        }
+    }
 }
 
 char* binarize_u(unsigned long long x)
 {
 
+    // Store chars in result and j for checking how much binary digits have x
+    char temp[64];
+    int j = 63;
+
+    // Initializing statement of result all zeros
+    for (int i = 0; i < 64; i++) {
+        temp[i] = '0';
+    }
+
+    /* While x is not equal to zero take its reminder.
+     *
+     * if it equals to 1 then result's jth cell is equal to 1
+     * Otherwise it equals to 0
+     *
+     * Decrease j and divide x by two
+    */
+    while (x != 0) {
+        if (x % 2) {
+            temp[j] = '1';
+        } else {
+            temp[j] = '0';
+        }
+        x /= 2;
+        j--;
+    }
+
+    char result[63-j];
+    char *res = result;
+
+    // Print digit to screen with spaces each 8 digits
+    for (int i = 0; j <= 63; j++) {
+        result[i] = temp[j];
+        i++;
+    }
+
+    return res;
 }
 
 char* binarize_s(signed long long y)
 {
-    /** YOUR CODE HERE */
+    if (y >= 0) {
+        return binarize_u(y);
+    } else {
+        y *= -1;
+        char* result = binarize_u(y);
 
+        two_complement(result);
+        return result;
+    }
 }
